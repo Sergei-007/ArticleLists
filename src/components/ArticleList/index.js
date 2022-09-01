c2dd8ba51f8363d25830876c4a829c90aba257a5
Thomas Peter <98786221+Sergei-007@users.noreply.github.com>
2022-09-01 10:01:06 -0500

    Initial commit

---
+++ src/components/ArticleList/index.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import SingleArticle from "../SingleArticle";
+import { useDispatch, useSelector } from "react-redux";
+import { loadArticles } from "../../store/articleReducer";
+
+const ArticleList = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.articleState.entries);
+
+  useEffect(() => {
+    dispatch(loadArticles());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Article List</h1>
+      {articles.map(({ id, title }) => (
+        <li key={id}>
+          <NavLink to={`/article/${id}`}>{title}</NavLink>
+        </li>
+      ))}
+
+      <Switch>
+        <Route path="/article/:id">
+          <SingleArticle />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default ArticleList;
